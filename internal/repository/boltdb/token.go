77878d5bb085df829bf669a1d1541aac5189c71d
v.sukhorukov <v.suhorukov@worksolutions.ru>
2021-10-30 00:37:00 +0300

    Отрефакторин репозитория для работы с токеном

--- internal/repository/boltdb/token.go
+++ internal/repository/boltdb/token.go
@@ -9,22 +9,22 @@ import (
 	"github.com/slavasukhorukov/pocketer_bot/internal/repository"
 )
 
-type TokenRepository struct {
+type tokenRepo struct {
 	db *bolt.DB
 }
 
-func NewTokenRepository(db *bolt.DB) *TokenRepository {
-	return &TokenRepository{db: db}
+func NewTokenRepository(db *bolt.DB) repository.TokenRepository {
+	return &tokenRepo{db: db}
 }
 
-func (repo *TokenRepository) Put(chatId int64, token string, bucket repository.Bucket) error {
+func (repo *tokenRepo) Put(chatId int64, token string, bucket repository.Bucket) error {
 	return repo.db.Update(func(tx *bolt.Tx) error {
 		b := tx.Bucket([]byte(bucket))
 		return b.Put(intToBytes(chatId), []byte(token))
 	})
 }
 
-func (repo *TokenRepository) Get(chatId int64, bucket repository.Bucket) (string, error) {
+func (repo *tokenRepo) Get(chatId int64, bucket repository.Bucket) (string, error) {
 	var token string
 	if err := repo.db.View(func(tx *bolt.Tx) error {
 		b := tx.Bucket([]byte(bucket))
